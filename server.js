108ef17eac823b374f0c2635fee0f967aba6e429
Rafael F Pessoa <rafaelpessoabr@gmail.com>
2024-11-12 22:13:28 -0300

    Atualização das Paginas

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Rotas
 const medicationRoutes = require('./routes/medicationRoutes')
 app.use('/api/medications', medicationRoutes)
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/pages', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
